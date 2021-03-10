73576bb354b2f18754babb47dba7ab1481303915
Cory Boyd <me@corytheboyd.com>
2021-03-10 16:18:47 -0700

    Add nil test for DateTime parser

--- lib/parser/date_time.rb
+++ lib/parser/date_time.rb
@@ -2,10 +2,11 @@ require 'date'
 
 module Parser
   class DateTime
-    # @param [String] value
+    # @param [String | nil] value
     #
-    # @return [String]
+    # @return [String | nil]
     def self.parse(value)
+      return value unless value.is_a?(String)
       ::DateTime.parse(value).iso8601
     end
   end
