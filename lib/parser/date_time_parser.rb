557e210de3b77a497149e7f6a51f847117079ea2
Cory Boyd <me@corytheboyd.com>
2021-03-10 17:36:51 -0700

    Fix bug when parsing mix of Y/M/D and M/D/Y dates

--- lib/parser/date_time_parser.rb
+++ lib/parser/date_time_parser.rb
@@ -11,7 +11,17 @@ module Parser
     def self.parse(value)
       return nil unless value.is_a?(String)
 
+      # Assume a M/D/Y formatted date
       month, day, year = value.split(/[^0-9]+/).map(&:to_i)
+
+      # Detect if it was actually a Y/M/D date and swap values
+      # accordingly
+      if month > 12
+        actual_day = year
+        year = month
+        month = day
+        day = actual_day
+      end
 
       # If we're dealing with anything less than four digit years,
       # interpret 00-21 as 2000-2021, and 22-99 as 1922-1999.
@@ -20,10 +30,6 @@ module Parser
         year += 2000
       elsif 22 <= year && year <= 99
         year += 1900
-      end
-
-      unless year && month && day
-        return nil
       end
 
       DateTime.new(year, month, day).iso8601
--- test/lib/parser/date_time.test.rb
+++ test/lib/parser/date_time.test.rb
@@ -4,10 +4,14 @@ class TestParser_DateTime < Minitest::Test
   def test_parse
     assert_nil Parser::DateTimeParser.parse(nil)
 
+    # When given M/D/Y formatted dates
     assert_equal '2021-01-01T00:00:00+00:00', Parser::DateTimeParser.parse('1/1/2021')
     assert_equal '2021-02-02T00:00:00+00:00', Parser::DateTimeParser.parse('2-2-2021')
 
-    # When given ambiguous dates, do our best to parse them
+    # When given Y/M/D formatted dates
+    assert_equal '2000-01-01T00:00:00+00:00', Parser::DateTimeParser.parse('2000/1/1')
+
+    # When given ambiguous dates, do our best
     assert_equal '1922-01-01T00:00:00+00:00', Parser::DateTimeParser.parse('1/1/22')
     assert_equal '1989-02-02T00:00:00+00:00', Parser::DateTimeParser.parse('2-2-89')
     assert_equal '2012-03-03T00:00:00+00:00', Parser::DateTimeParser.parse('3/3/12')
