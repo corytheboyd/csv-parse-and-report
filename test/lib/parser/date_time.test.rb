8e9e759905629a7e8cfd25a0b96f849d76ed7dc7
Cory Boyd <me@corytheboyd.com>
2021-03-10 17:18:48 -0700

    Add a couple more possible date formats to test

--- test/lib/parser/date_time.test.rb
+++ test/lib/parser/date_time.test.rb
@@ -9,8 +9,8 @@ class TestParser_DateTime < Minitest::Test
 
     # When given ambiguous dates, do our best to parse them
     assert_equal '1922-01-01T00:00:00+00:00', Parser::DateTimeParser.parse('1/1/22')
-    assert_equal '1989-02-02T00:00:00+00:00', Parser::DateTimeParser.parse('2/2/89')
+    assert_equal '1989-02-02T00:00:00+00:00', Parser::DateTimeParser.parse('2-2-89')
     assert_equal '2012-03-03T00:00:00+00:00', Parser::DateTimeParser.parse('3/3/12')
-    assert_equal '2021-04-04T00:00:00+00:00', Parser::DateTimeParser.parse('4/4/21')
+    assert_equal '2021-04-04T00:00:00+00:00', Parser::DateTimeParser.parse('4.4.21')
   end
 end
